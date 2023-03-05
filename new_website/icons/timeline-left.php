da0e8cd06e54122eae0205a6c10d3c70e41ff3e9
Phoenix <phoenixn82@gmail.com>
2023-03-05 11:10:35 -0600

    Add rest of resume page

---
+++ new_website/icons/timeline-left.php
@@ -0,0 +1,87 @@
+<section class="timeline-left">
+     <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="100%" height="100%" viewBox="0 0 8794 3905" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+  <path d="M13.542,2612.58l970.098,-0l643.76,726.063l389.476,0" style="fill:#505050;fill-opacity:0;stroke:#fff;stroke-width:20.83px;" />
+  <path d="M4807.13,296.689c-0,-148.799 -120.807,-269.606 -269.606,-269.606l-789.363,0c-148.8,0 -269.606,120.807 -269.606,269.606l0,539.212c0,148.8 120.806,269.606 269.606,269.606l789.363,0c148.799,0 269.606,-120.806 269.606,-269.606l-0,-539.212Z" style="fill:#151619;" />
+  <path d="M4834.22,296.689c-0,-163.747 -132.942,-296.689 -296.689,-296.689l-789.363,0c-163.747,0 -296.689,132.942 -296.689,296.689c-0,0 -0,539.212 -0,539.212c-0,163.748 132.942,296.69 296.689,296.69c0,-0 789.363,-0 789.363,-0c163.747,-0 296.689,-132.942 296.689,-296.69l-0,-539.212Zm-27.083,0c-0,-148.799 -120.807,-269.606 -269.606,-269.606l-789.363,0c-148.8,0 -269.606,120.807 -269.606,269.606l0,539.212c0,148.8 120.806,269.606 269.606,269.606l789.363,0c148.799,0 269.606,-120.806 269.606,-269.606l-0,-539.212Z" style="fill:#fff;" />
+  <path d="M3345.45,3069.03c-0,-148.799 -120.807,-269.606 -269.606,-269.606l-789.363,0c-148.8,0 -269.606,120.807 -269.606,269.606l-0,539.212c-0,148.8 120.806,269.606 269.606,269.606l789.363,0c148.799,0 269.606,-120.806 269.606,-269.606l-0,-539.212Z" style="fill:#151619;" />
+  <path d="M3372.53,3069.03c-0,-163.747 -132.942,-296.689 -296.689,-296.689l-789.363,-0c-163.747,-0 -296.689,132.942 -296.689,296.689c-0,0 -0,539.212 -0,539.212c-0,163.748 132.942,296.69 296.689,296.69l789.363,-0c163.747,-0 296.689,-132.942 296.689,-296.69l-0,-539.212Zm-27.083,0c-0,-148.799 -120.807,-269.606 -269.606,-269.606l-789.363,0c-148.8,0 -269.606,120.807 -269.606,269.606l-0,539.212c-0,148.8 120.806,269.606 269.606,269.606l789.363,0c148.799,0 269.606,-120.806 269.606,-269.606l-0,-539.212Z" style="fill:#fff;" />
+  <path d="M13.542,2171.65l812.826,0l207.055,220.062l1456.95,-0l314.847,-334.626l813.304,0" style="fill:none;stroke:#fff;stroke-width:27.08px;" />
+  <path d="M3590.85,2057.1l863.886,-0l220.062,220.061l1548.47,0l334.626,-334.625l864.393,-0" style="fill:none;stroke:#fff;stroke-width:27.08px;" />
+  <path d="M15.351,1602.4l517.686,0l410.422,410.422l1274.36,-0l384.33,-384.33l0,-792.845l269.403,-269.402l586.287,0" style="fill:none;stroke:#fff;stroke-width:27.08px;" />
+  <rect x="7430.36" y="1897.16" width="92.046" height="92.046" style="fill:none;stroke:#fff;stroke-width:10.42px;" />
+  <path d="M2085.96,1214.85l-68.674,-61.29l-61.289,68.674l68.674,61.29l61.289,-68.674Z" style="fill:none;stroke:#fff;stroke-width:10.42px;" />
+  <path d="M700.037,3262.59l-29.19,-87.296l-87.295,29.189l29.189,87.296l87.296,-29.189Z" style="fill:none;stroke:#fff;stroke-width:10.42px;" />
+  <g>
+    <path d="M2949.2,3661.36l-0,22.742l-18.095,0l18.095,0l-0,24.522l-17.304,0l17.304,0l-0,24.522l-16.019,0l16.019,0l-0,24.127l-18.194,-0l18.194,-0l-0,24.819l-17.614,-0l17.614,-0l-0,24.206l24.669,0l-0,18.827l-0,-18.827l24.927,0l-0,18.09l-0,-18.09l23.251,0l-0,18.508l-0,-18.508l23.951,0l-0,18.138l-0,-18.138l24.967,0l-0,18.99l-0,-18.99l23.443,0l0,-24.358l20.382,0l-20.382,0l0,-24.021l19.27,-0l-19.27,-0l0,-24.034l16.811,-0l-16.811,-0l0,-23.959l19.326,-0l-19.326,-0l0,-24.501l18.119,0l-18.119,0l0,-23.329l-23.31,-0l-0,-20.281l-0,20.281l-24.7,-0l0,-19.646l0,19.646l-24.524,-0l-0,-20.531l-0,20.531l-24.126,-0l-0,-20.288l-0,20.288l-23.999,-0l-0,-19.854l-0,19.854l-24.549,-0l-0,-0.736Z" style="fill:none;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-linejoin:miter;" />
+    <rect x="3030.28" y="3741.83" width="8.006" height="8.024" style="fill:#fff;" />
+    <path d="M3039.28,3741.83c0,-0.552 -0.447,-1 -1,-1l-8.005,-0c-0.553,-0 -1,0.448 -1,1l-0,8.024c-0,0.552 0.447,1 1,1l8.005,0c0.553,0 1,-0.448 1,-1l0,-8.024Zm-1,-0l-8.005,-0l-0,8.024l8.005,0l0,-8.024Z" style="fill:#fff;" />
+    <rect x="3005.99" y="3741.83" width="8.006" height="8.024" style="fill:#fff;" />
+    <path d="M3014.99,3741.83c0,-0.552 -0.447,-1 -1,-1l-8.005,-0c-0.553,-0 -1,0.448 -1,1l-0,8.024c-0,0.552 0.447,1 1,1l8.005,0c0.553,0 1,-0.448 1,-1l0,-8.024Zm-1,-0l-8.005,-0l-0,8.024l8.005,0l0,-8.024Z" style="fill:#fff;" />
+    <rect x="3005.99" y="3717.61" width="8.006" height="8.024" style="fill:#fff;" />
+    <path d="M3014.99,3717.61c0,-0.552 -0.447,-1 -1,-1l-8.005,0c-0.553,0 -1,0.448 -1,1l-0,8.025c-0,0.552 0.447,1 1,1l8.005,-0c0.553,-0 1,-0.448 1,-1l0,-8.025Zm-1,0l-8.005,0l-0,8.025l8.005,-0l0,-8.025Z" style="fill:#fff;" />
+    <rect x="3030.28" y="3717.61" width="8.006" height="8.024" style="fill:#fff;" />
+    <path d="M3039.28,3717.61c0,-0.552 -0.447,-1 -1,-1l-8.005,0c-0.553,0 -1,0.448 -1,1l-0,8.025c-0,0.552 0.447,1 1,1l8.005,-0c0.553,-0 1,-0.448 1,-1l0,-8.025Zm-1,0l-8.005,0l-0,8.025l8.005,-0l0,-8.025Z" style="fill:#fff;" />
+  </g>
+  <g>
+    <g>
+      <path d="M4450.73,1037.64l0,-17.785c0,0 0.197,-7.275 6.403,-9.365" style="fill:#fff;fill-opacity:0;stroke:#fff;stroke-width:8.33px;" />
+      <path d="M4457.14,1010.49l99.617,-0c-0,-0 10.336,-2.56 9.918,-13.219c-0.419,-10.659 -10.387,-10.213 -10.387,-10.213l-63.022,0c-0,0 -9.972,-1.323 -9.419,-12.514c0.553,-11.19 17.35,-9.121 17.35,-9.121l60.486,0c0,0 5.063,1.622 5.063,-8.218l0,-81.645c0,-0 -0.228,-4.657 -4.803,-4.803c-4.574,-0.146 -154.271,0 -154.271,0c0,0 -6.522,-0.401 -6.522,6.523l-0,82.334c-0,0 -0.381,6.112 6.112,6.112l43.63,-0" style="fill:none;stroke:#fff;stroke-width:8.33px;" />
+    </g>
+    <g>
+      <rect x="4418.94" y="938.999" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4441.76" y="938.999" width="51.505" height="7.643" style="fill:#fff;" />
+      <rect x="4418.97" y="916.805" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4418.97" y="894.757" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4441.25" y="916.805" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4441.25" y="894.757" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4463.29" y="916.805" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4463.29" y="894.757" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4485.58" y="916.805" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4485.58" y="894.757" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4507.77" y="916.805" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4507.77" y="938.999" width="8.158" height="7.643" style="fill:#fff;" />
+      <rect x="4529.96" y="917.029" width="18.595" height="7.643" style="fill:#fff;" />
+      <rect x="4529.96" y="894.869" width="18.595" height="7.643" style="fill:#fff;" />
+      <path d="M4548.55,946.642l0,-21.97l-7.643,-0l0,21.97l7.643,-0Z" style="fill:#fff;" />
+      <rect x="4507.77" y="894.757" width="8.158" height="7.643" style="fill:#fff;" />
+    </g>
+  </g>
+  <text class="svg-text" x="2213.76px" y="3814.09px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:233.821px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">2021</text>
+  <text class="svg-text" x="3675.44px" y="1034.84px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:233.821px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">2023</text>
+  <g transform="matrix(1,0,0,1,426.228,-67502.5)">
+    <text class="svg-text" x="1867.11px" y="70811.7px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">Gr <tspan x="1986.14px 2045.72px " y="70811.7px 70811.7px ">ad</tspan>uat <tspan x="2262.56px 2324.4px " y="70811.7px 70811.7px ">ed</tspan> fr <tspan x="2484.17px 2548.16px " y="70811.7px 70811.7px ">om</tspan>
+    </text>
+    <text class="svg-text" x="1717.53px" y="70934.3px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">Magnolia High-school</text>
+  </g>
+  <g transform="matrix(1,0,0,1,569.345,-67444.6)">
+    <text class="svg-text" x="2944.71px" y="67982.2px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">Complet <tspan x="3364.7px 3426.53px " y="67982.2px 67982.2px ">ed</tspan> ment <tspan x="3773.61px 3837.59px " y="67982.2px 67982.2px ">or</tspan>ship at </text>
+    <text class="svg-text" x="3078.81px" y="68104.8px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">P <tspan x="3144.22px 3206.05px " y="68104.8px 68104.8px ">er</tspan>petual Education </text>
+  </g>
+  <path d="M13.542,3065.13l570.001,-0l40.442,117.05" style="fill:none;stroke:#fff;stroke-width:27.08px;" />
+  <path d="M1982.35,1257.16l-270.033,339.264l-328.124,-0l-197.054,-247.575l-1171.79,-0" style="fill:none;stroke:#fff;stroke-width:27.08px;" />
+  <path d="M8766.02,1672.73c-0,-148.8 -120.807,-269.606 -269.606,-269.606l-789.363,-0c-148.799,-0 -269.606,120.806 -269.606,269.606l0,539.212c0,148.799 120.807,269.606 269.606,269.606l789.363,-0c148.799,-0 269.606,-120.807 269.606,-269.606l-0,-539.212Z" style="fill:#151619;" />
+  <path d="M8793.1,1672.73c0,-163.748 -132.942,-296.69 -296.689,-296.69c-0,0 -789.363,0 -789.363,0c-163.747,0 -296.689,132.942 -296.689,296.69l-0,539.212c-0,163.747 132.942,296.689 296.689,296.689c0,0 789.363,0 789.363,0c163.747,0 296.689,-132.942 296.689,-296.689c0,-0 0,-539.212 0,-539.212Zm-27.083,-0c-0,-148.8 -120.807,-269.606 -269.606,-269.606l-789.363,-0c-148.799,-0 -269.606,120.806 -269.606,269.606l0,539.212c0,148.799 120.807,269.606 269.606,269.606l789.363,-0c148.799,-0 269.606,-120.807 269.606,-269.606l-0,-539.212Z" style="fill:#fff;" />
+  <text class="svg-text" x="7647.61px" y="2411.35px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:233.821px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">2023</text>
+  <g>
+    <path d="M8505,2414.15l-13.642,0c-0,0 -8.119,-9.875 -8.119,-33.587l52.372,0c0,0 8.925,-1.827 8.925,-8.925l0,-115.938c0,0 -2.761,-8.489 -10.941,-8.489l-144.498,-0c0,-0 -10.007,-0.674 -10.007,10.007l0,114.424c0,0 -0.888,7.723 7.723,7.723l51.506,0c0,0 2.379,23.923 -8.362,34.664l-14.181,0l14.181,0l61.401,0.121" style="fill:none;stroke:#fff;stroke-width:3.33px;" />
+    <path d="M8438.32,2379.37l44.92,1.198" style="fill:none;stroke:#fff;stroke-width:3.33px;" />
+    <path d="M8379.07,2346.93l165.472,-0" style="fill:none;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8418.64" y="2315.81" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8418.64" y="2293.55" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8396.34" y="2293.55" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8396.34" y="2271.63" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8418.64" y="2271.63" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8457.19" y="2359.91" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+    <rect x="8396.34" y="2315.81" width="7.643" height="7.643" style="fill:#fff;stroke:#fff;stroke-width:3.33px;" />
+  </g>
+  <g transform="matrix(1,0,0,1,4527.93,-66119.6)">
+    <text class="svg-text" x="3298.15px" y="67982.2px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">Complet <tspan x="3718.14px 3779.98px " y="67982.2px 67982.2px ">ed</tspan>
+    </text>
+    <text class="svg-text" x="3146.96px" y="68104.8px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">Br <tspan x="3252.76px 3312.33px " y="68104.8px 68104.8px ">az</tspan>os F <tspan x="3575.41px 3634.98px 3692.29px 3721.48px 3783.32px " y="68104.8px 68104.8px 68104.8px 68104.8px 68104.8px ">asten</tspan>ers </text>
+    <text class="svg-text" x="3372.44px" y="68227.4px" style="font-family:'HelveticaNeue-Thin', 'Helvetica Neue', sans-serif;font-weight:200;font-size:119.146px;fill:#fff;stroke:#fff;stroke-width:2.29px;stroke-linecap:butt;stroke-miterlimit:2;">W <tspan x="3475.5px 3537.34px " y="68227.4px 68227.4px ">eb</tspan>sit <tspan x="3710.1px " y="68227.4px ">e</tspan>
+    </text>
+  </g>
+</svg>
+    </section>
