d6c1da19fc2266296aa69b9f4ea79bd4b278a7b7
Phoenix <phoenixn82@gmail.com>
2023-03-06 11:17:45 -0600

    Adjusted size of footer icons

--- new_website/footer.php
+++ new_website/footer.php
@@ -4,11 +4,11 @@
     <footer>
         
           <div class="footer-icon left">
-              <svg width="100%" height="100%" viewBox="0 0 2860 242" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path d="M2859.25,0.5l-109.865,240.726l-2748.89,0l0,-240.726l2858.75,0Z" style="fill:#fff;fill-rule:nonzero;stroke:#000;stroke-opacity:0;stroke-width:1px;"/></svg>
+              <svg width="100%" height="2.5vh" preserveAspectRatio="xMidYMid meet" viewBox="0 0 2860 242" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path d="M2859.25,0.5l-109.865,240.726l-2748.89,0l0,-240.726l2858.75,0Z" style="fill:#fff;fill-rule:nonzero;stroke:#000;stroke-opacity:0;stroke-width:1px;"/></svg>
           </div>
 
           <div class="footer-icon right">
-              <svg width="100%" height="100%" viewBox="0 0 2860 242" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path d="M0.5,0.5l109.865,240.726l2748.89,0l-0,-240.726l-2858.75,0Z" style="fill:#fff;fill-rule:nonzero;stroke:#000;stroke-opacity:0;stroke-width:1px;"/></svg>
+              <svg width="100%" height="2.5vh" preserveAspectRatio="xMidYMid meet" viewBox="0 0 2860 242" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path d="M0.5,0.5l109.865,240.726l2748.89,0l-0,-240.726l-2858.75,0Z" style="fill:#fff;fill-rule:nonzero;stroke:#000;stroke-opacity:0;stroke-width:1px;"/></svg>
           </div>
 
           <div class="footer-icon-phone">
