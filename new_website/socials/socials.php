287cc36c292a9d215ffcd28ea9e54a1dbaf2cca9
Phoenix <phoenixn82@gmail.com>
2023-03-06 14:38:40 -0600

    Adjusted social icons phone size

--- new_website/socials/socials.php
+++ new_website/socials/socials.php
@@ -1,9 +1,9 @@
 <section class="inner-column">
 	<div class="icon-row-socials">
-		<div class='socials linkdin'><a href="www.linkedin.com/in/phoenix-nachtegaele
+		<div class='socials-links linkdin'><a href="www.linkedin.com/in/phoenix-nachtegaele
 " target="_blank"><?php include("icons/linkdin-icon.php"); ?></a></div>
-		<div class='socials github'><a href="https://github.com/Phoenixn82" target="_blank"><?php include("icons/github-icon.php"); ?></a></div>
-		<div class='socials codepen'><a href="https://codepen.io/phoenixn82" target="_blank"><?php include("icons/codepen-icon.php"); ?></a></div>
+		<div class='socials-links github'><a href="https://github.com/Phoenixn82" target="_blank"><?php include("icons/github-icon.php"); ?></a></div>
+		<div class='socials-links codepen'><a href="https://codepen.io/phoenixn82" target="_blank"><?php include("icons/codepen-icon.php"); ?></a></div>
 		
 	</div>
 </section>
