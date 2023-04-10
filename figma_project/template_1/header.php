b4f8244da4a487733f09eb59debe47a46fc2d4d8
Phoenix <phoenixn82@gmail.com>
2023-04-10 15:43:26 -0500

    Adding Figma Project file structure

---
+++ figma_project/template_1/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Template</title>
+	<link rel="stylesheet" href="site.css">
+
+</head>
+<body>
+<header>
+	<div class="header-nav">
+		<a class='home-link' href="#">Home</a>
+		<a href="#">Updates</a>
+		<a href="#">Services</a>
+		<a href="#">Features</a>
+		<a href="#">About Us</a>
+	</div>
+</header>
+<main>
